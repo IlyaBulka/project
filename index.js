1701257211d4c34dfa8cb12fabd04a7f5533226b
Ilya <ilyabulka2@gmail.com>
2022-02-06 12:31:23 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const clients = [{
+        id: 1,
+        name: 'Аристарх',
+        lastName: 'Петров',
+    },
+    {
+        id: 2,
+        name: 'Семен',
+        lastName: 'Куленков',
+    },
+    {
+        id: 3,
+        name: 'Петя',
+        lastName: 'Шпаков',
+    },
+    {
+        id: 4,
+        name: 'Артём',
+        lastName: 'Артров',
+    },
+    {
+        id: 5,
+        name: 'Аркадий',
+        lastName: 'Капронов',
+    },
+    {
+        id: 6,
+        name: 'Генадий',
+        lastName: 'Лебедев',
+    },
+    {
+        id: 7,
+        name: 'Слава',
+        lastName: 'Копронов',
+    },
+    {
+        id: 8,
+        name: 'Екатерина',
+        lastName: 'Решетковна',
+    },
+    {
+        id: 9,
+        name: 'Слава',
+        lastName: 'Украине',
+    },
+    {
+        id: 10,
+        name: 'Антон',
+        lastName: 'Хлопков',
+    },
+    {
+        id: 11,
+        name: 'Александр',
+        lastName: 'Кукушкин',
+    },
+    {
+        id: 12,
+        name: 'Ольга',
+        lastName: 'Картошкина',
+    },
+    {
+        id: 13,
+        name: 'Лелуш',
+        lastName: 'Ламперуш',
+    },
+    {
+        id: 14,
+        name: 'Альфонс',
+        lastName: 'Элрик',
+    },
+    {
+        id: 15,
+        name: 'Эдвард"',
+        lastName: 'элрик',
+    }
+]
